2f09eca915f67fa50c355fd5a1a12aaecab69303
jason <jason@dotjson.net>
2024-07-27 21:11:32 -0600

    comment

--- rc.lua
+++ rc.lua
@@ -507,9 +507,7 @@ awful.rules.rules = {
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function (c)
-    -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
+    -- give windows rounded edges
     c.shape = function(cr,w,h)
         gears.shape.rounded_rect(cr, w, h, 10)
     end
